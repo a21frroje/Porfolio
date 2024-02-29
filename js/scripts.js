5110499dbc73a598a300e8e41b1c05e21d62d4a2
Jesus Frias Rojas <a21frroje@iesgrancapitan.org>
2024-02-29 13:00:54 +0100

    finis

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,42 @@
-const toggleTheme = document.getElementById("toggle-theme");
-const toggleText = document.getElementById("toggle-text");
-const toggleColors = document.getElementById("toggle-colors");
-const toggleIcon = document.getElementById("toggle-icon")
+{
+    const toggleTheme = document.getElementById("toggle-theme");
+    const toggleText = document.getElementById("toggle-text");
+    const toggleColors = document.getElementById("toggle-colors");
+    const toggleIcon = document.getElementById("toggle-icon")
 
-const rootStyles = document.documentElement.style;
+    const rootStyles = document.documentElement.style;
 
-const flagsElement = document.getElementById("flags")
+    const flagsElement = document.getElementById("flags")
 
-const textsToChange = document.querySelectorAll("[data-section]");
+    const textsToChange = document.querySelectorAll("[data-section]");
 
-const changeLenguage = async(lenguage) =>{
-    const requestJson = await fetch(`./languages/${lenguage}.json`);
-    const texts = await requestJson.json();
+    const changeLenguage = async(lenguage) =>{
+        const requestJson = await fetch(`./languages/${lenguage}.json`);
+        const texts = await requestJson.json();
 
-    for(const textToChange of textsToChange){
-        const section = textToChange.dataset.section;
-        const value = textToChange.dataset.value;
-        textToChange.innerHTML=texts[section][value]
+        for(const textToChange of textsToChange){
+            const section = textToChange.dataset.section;
+            const value = textToChange.dataset.value;
+            textToChange.innerHTML=texts[section][value]
+        }
     }
+
+    flagsElement.addEventListener('click', (e)=>{
+        changeLenguage(e.target.parentElement.dataset.lenguage);
+    })
+
+    toggleTheme.addEventListener('click', ()=>{
+        document.body.classList.toggle("dark");
+        if(toggleIcon.src.includes("moon.svg")){
+            toggleIcon.src = "assets/icons/sun.svg";
+            toggleText.textContent = "Ligth Mode";
+        }else{
+            toggleIcon.src = "assets/icons/moon.svg";
+            toggleText.textContent = "Dark Mode";
+        }
+    })
+
+    toggleColors.addEventListener("click", (e)=>{
+        rootStyles.setProperty("--primary-color", e.target.dataset.color)
+    })
 }
-
-flagsElement.addEventListener('click', (e)=>{
-    changeLenguage(e.target.parentElement.dataset.lenguage);
-})
-
-toggleTheme.addEventListener('click', ()=>{
-    document.body.classList.toggle("dark");
-    if(toggleIcon.src.includes("moon.svg")){
-        toggleIcon.src = "assets/icons/sun.svg";
-        toggleText.textContent = "Ligth Mode";
-    }else{
-        toggleIcon.src = "assets/icons/moon.svg";
-        toggleText.textContent = "Dark Mode";
-    }
-})
-
-toggleColors.addEventListener("click", (e)=>{
-    rootStyles.setProperty("--primary-color", e.target.dataset.color)
-})
